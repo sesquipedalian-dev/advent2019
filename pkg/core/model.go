07f63bdabaca10dde4d3680e4aa157808bb407ce
Scott <sesquipedalian.dev@gmail.com>
2020-11-20 21:55:43 -0700

    flesh out default handler some

--- pkg/core/model.go
+++ pkg/core/model.go
@@ -1,6 +1,8 @@
 package core
 
 import (
+	"fmt"
+	"io/ioutil"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
@@ -21,9 +23,27 @@ type Challenge struct {
 }
 
 func ConfigureDays() *Days {
-	return &Days{}
+	return &Days{
+		Days: map[string]Day{
+			"1": Day{
+				ID: "1",
+				Challenges: map[string]Challenge{
+					"1": Challenge{
+						ID:      "1",
+						Handler: defaultHandler,
+					},
+				},
+			},
+		},
+	}
 }
 
 func defaultHandler(c echo.Context) error {
-	return c.String(http.StatusNotImplemented, "Not Implemented")
+	reqBodyBytes, ok := ioutil.ReadAll(c.Request().Body)
+	if ok != nil {
+		return c.String(http.StatusBadRequest, "Can't read request body")
+	}
+	reqBodyString := string(reqBodyBytes)
+	return c.String(http.StatusNotImplemented,
+		fmt.Sprintf("Not Implemented %s", reqBodyString))
 }
